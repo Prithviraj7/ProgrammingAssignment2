d40ddeaba27e3c30ab95be1c1bb1e1d4a84ffb80
Prithviraj Dhar <prithvirj95@gmail.com>
2015-01-25 02:49:50 +0530

    newest

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,29 @@
 makeCacheMatrix <- function(x = matrix()) { 
   m<-NULL               
   set<-function(y){
-    x<<-y #the given matrix gets stored in x
+    x<<-y #the given matrix gets stored in x ie value of input matrix is set
     m<<-NULL
   }
-  get<-function() x
-  setmatrix<-function(solve) m<<- solve # now m = inverse of given matrix
-  getmatrix<-function() m
+  get<-function() x #get the value of input matrix
+  setmatrix<-function(solve) m<<- solve # now set inverse of given matrix
+  getmatrix<-function() m #get the inverse of given matrix
   list(set=set, get=get,
        setmatrix=setmatrix,
        getmatrix=getmatrix)
 }
 
 cacheSolve <- function(x=matrix(), ...) {
-  m<-x$getmatrix()
-  if(!is.null(m)){ # check if the matrix is null or not
-    message("getting cached data")
-    return(m)
+  m<-x$getmatrix() #caclulate the inverse of given matrix
+  if(!is.null(m)){ # check if the inverse has already been calculated or not
+    message("getting cached data") #
+    return(m) # if yes then inverse from the cache is retrieved
   }
-  matrix<-x$get()
-  m <- solve(matrix, ...)
-  x$setmatrix(m)
+  matrix<-x$get() # else, get the value of input matrix
+  m <- solve(matrix, ...) #computation of inverse of the input matrix
+  x$setmatrix(m) #now I am setting the value of inverse of the input matrix
   m
 }
 #example
-#a<-makeCacheMatrix()---initialize
-#a$set(matrix(1:4,2,2))---input matrix
+#a<-makeCacheMatrix()
+#a$set(matrix(1:4,2,2))
 #cacheSolve(a)      
